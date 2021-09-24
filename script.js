06f348af5ef601f733f55f93f9ac94f45e70d5fc
fintonius <fintonius.mcginty@gmail.com>
2021-09-24 09:24:56 +0100

    added slidebar, changed userInput to take value from slider, styled slider

--- script.js
+++ script.js
@@ -10,8 +10,16 @@
 
 //create a function that takes in the user input and creates a grid of divs heights & width of input
 
-var userNumber = prompt('How many squares?');
+//var userNumber = prompt('How many squares?');
 //var squareNumber = userNumber * userNumber;
+var slider = document.getElementById("userInput");
+var output = document.getElementById("output");
+output.innerHTML = slider.value;
+var userNumber = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
 
 function createDiv() {
     var squareDiv = document.createElement('div');
@@ -38,4 +46,4 @@ function createGrid() {
 
 createGrid();
 
-//console.log(squareNumber);
+console.log(userNumber);
