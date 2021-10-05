e247a9c3e03461eb426468f9dba1b22c12385bf6
fintonius <fintonius.mcginty@gmail.com>
2021-10-05 19:04:29 +0100

    streamlined grid creation down to 1 function, have random colour button working but not generating a new colour every div. That is next.

---
+++ oldGuff.js
@@ -0,0 +1,91 @@
+//create a grid of 16 divs. 4 x 4. 
+//add a button which will clear the grid and ask the user how many
+//squares PER SIDE the new grid should contain.
+
+//NEW BRANCH: need to have a variable to cast current state into? Don't really know what I'm doing. 
+//I think it might be best to branch and restart TBH. It feels like it's a lot of patching
+//the current script and finding convoluted work arounds to current stuff to "make" it work
+
+var slider = document.getElementById("slider");
+var output1 = document.getElementById("output1");
+var output2 = document.getElementById("output2");
+var box = document.getElementById('wrapper');
+var rainbowButton = document.getElementById("rainbow");
+var rainbow = '';
+var eraser = ''; //the button should stay 'clicked' for this. 'Unclicking' reverts to colouring
+var userNumber = slider.value;
+output1.textContent = slider.value;
+output2.textContent = slider.value;
+
+//Using a class for 'output' returns an array which was overly complicated to add the slider value to
+//so just went with 2 id instead.
+slider.oninput = function () { 
+    output1.textContent = this.value; 
+    output2.textContent = this.value;   
+}
+
+slider.onchange = function () {
+    userNumber = this.value;//SO the grid needs to be adjustable, needs to work with changing user input.
+    //A function that squares the number and creates that amount of divs.
+    //THE SIZE OF THE OVERALL GRID NEEDS TO REMAIN THE SAME, eg: ALWAYS 800px WIDE
+    
+    // **remember to give the divs a small border just to see what's being made :) **
+    
+    //create a function that takes in the user input and creates a grid of divs heights & width of input
+    
+    //var userNumber = prompt('How many squares?');
+    //var squareNumber = userNumber * userNumber;
+    createGrid();
+}
+function rainbowClick() {
+    rainbow = 'true';
+    createGrid();
+}
+function rainbowDiv() {
+  
+   var redDiv = document.createElement('div');
+   redDiv.className = 'square';
+   redDiv.addEventListener("mouseover", function(event) {
+       event.target.style.backgroundColor = "red";
+   });   
+   return redDiv;
+};
+
+function createDiv() {   
+    var squareDiv = document.createElement('div');
+    squareDiv.className = 'square';
+    squareDiv.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "orange";
+    });
+    return squareDiv;
+};
+
+//only 'resizing' grid should reset? Changing colour should keep current drawing
+function createGrid() {
+    box.innerHTML = ``;
+    let myDivs = [];
+    let i = 0;
+    wrapper.style.gridTemplateColumns = `repeat(${userNumber}, 1fr)`
+    wrapper.style.gridTemplateRows = `repeat(${userNumber}, 1fr)`
+
+    if (rainbow === 'true') {
+        console.log('getting there');
+        for (i; i <= userNumber * userNumber; i++) {
+            myDivs.push(rainbowDiv());
+            box.appendChild(myDivs[i]);
+        }
+    } else {
+    for (i; i <= userNumber * userNumber; i++) {
+        myDivs.push(createDiv());
+        box.appendChild(myDivs[i]);
+    }
+    }
+}
+
+function resetFunction() {
+    box.innerHTML = ``;
+    createGrid();
+}
+
+createGrid();
+console.log(userNumber);
--- script.js
+++ script.js
@@ -1,91 +1,70 @@
-//create a grid of 16 divs. 4 x 4. 
-//add a button which will clear the grid and ask the user how many
-//squares PER SIDE the new grid should contain.
+//PSEUDO CODE:
+//window on load create box containing default 16 x 16 grid of divs
+//mouse-over divs change-color to black
+//slider input to change div grid: n x n amount
+//button to reset grid to default size
+//button to change color from black to random
+//button to activate eraser option
+//button to allow color picker option
 
-//NEW BRANCH TEST: need to have a variable to cast current state into? Don't really know what I'm doing. 
-//I think it might be best to branch and restart TBH. It feels like it's a lot of patching
-//the current script and finding convoluted work arounds to current stuff to "make" it work
+//funtion to create div with classname & id if needed
 
-var slider = document.getElementById("slider");
-var output1 = document.getElementById("output1");
-var output2 = document.getElementById("output2");
-var box = document.getElementById('wrapper');
-var rainbowButton = document.getElementById("rainbow");
-var rainbow = '';
-var eraser = ''; //the button should stay 'clicked' for this. 'Unclicking' reverts to colouring
-var userNumber = slider.value;
-output1.textContent = slider.value;
-output2.textContent = slider.value;
+//function to create div grid by multiplying above div. Has a default setting: 16 x 16. 
+    //Take user input through slider to change amount of columns & rows
 
-//Using a class for 'output' returns an array which was overly complicated to add the slider value to
-//so just went with 2 id instead.
-slider.oninput = function () { 
-    output1.textContent = this.value; 
-    output2.textContent = this.value;   
-}
+//function to allow random-color change for mouse-over action
+    //when the grid is created are the divs stored in a way that is accessible by this function?
+    //add eventlistener to the div when it's created - if (rainbowClick === 'true') {}  else {} ?
 
-slider.onchange = function () {
-    userNumber = this.value;//SO the grid needs to be adjustable, needs to work with changing user input.
-    //A function that squares the number and creates that amount of divs.
-    //THE SIZE OF THE OVERALL GRID NEEDS TO REMAIN THE SAME, eg: ALWAYS 800px WIDE
+    const defaultSize = '';
+    const defaultColor = '';
+
+    var currentColor = `var(--black)`;
+
+    var slider = document.getElementById("slider");
+    var output1 = document.getElementById("output1");
+    var output2 = document.getElementById("output2");
+    var box = document.getElementById('wrapper');
+    var rainbowButton = document.getElementById("rainbow");
+    var rainbow = '';
+    var eraser = ''; //the button should stay 'clicked' for this. 'Unclicking' reverts to colouring
+    var userNumber = slider.value;
+    var squareDiv = '';
+    output1.textContent = slider.value;
+    output2.textContent = slider.value;
+
+    slider.oninput = function () { 
+        output1.textContent = this.value; 
+        output2.textContent = this.value;   
+    }
     
-    // **remember to give the divs a small border just to see what's being made :) **
-    
-    //create a function that takes in the user input and creates a grid of divs heights & width of input
-    
-    //var userNumber = prompt('How many squares?');
-    //var squareNumber = userNumber * userNumber;
-    createGrid();
-}
-function rainbowClick() {
-    rainbow = 'true';
-    createGrid();
-}
-function rainbowDiv() {
-  
-   var redDiv = document.createElement('div');
-   redDiv.className = 'square';
-   redDiv.addEventListener("mouseover", function(event) {
-       event.target.style.backgroundColor = "red";
-   });   
-   return redDiv;
-};
+    slider.onchange = function () {
+        userNumber = this.value;
+        createGrid();
+    }
 
-function createDiv() {   
-    var squareDiv = document.createElement('div');
-    squareDiv.className = 'square';
-    squareDiv.addEventListener("mouseover", function(event) {
-        event.target.style.backgroundColor = "orange";
-    });
-    return squareDiv;
-};
+    function createGrid() {   
+        box.innerHTML = '';    
+        wrapper.style.gridTemplateColumns = `repeat(${userNumber}, 1fr)`;
+        wrapper.style.gridTemplateRows = `repeat(${userNumber}, 1fr)`;
 
-//only 'resizing' grid should reset? Changing colour should keep current drawing
-function createGrid() {
-    box.innerHTML = ``;
-    let myDivs = [];
-    let i = 0;
-    wrapper.style.gridTemplateColumns = `repeat(${userNumber}, 1fr)`
-    wrapper.style.gridTemplateRows = `repeat(${userNumber}, 1fr)`
-
-    if (rainbow === 'true') {
-        console.log('getting there');
+        let i = 0;
         for (i; i <= userNumber * userNumber; i++) {
-            myDivs.push(rainbowDiv());
-            box.appendChild(myDivs[i]);
+            squareDiv = document.createElement('div');
+            squareDiv.addEventListener("mouseover", function(event) {
+                event.target.style.backgroundColor = currentColor;
+            });
+            box.appendChild(squareDiv);
         }
-    } else {
-    for (i; i <= userNumber * userNumber; i++) {
-        myDivs.push(createDiv());
-        box.appendChild(myDivs[i]);
-    }
-    }
-}
+        };
 
-function resetFunction() {
-    box.innerHTML = ``;
+    function rainbowClick() {
+        
+        //code from michalosman's etch-a-sketch
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        console.log(randomR, randomB, randomG);
+        currentColor = `RGB(${randomR}, ${randomG}, ${randomB})`
+    }
     createGrid();
-}
-
-createGrid();
-console.log(userNumber);
